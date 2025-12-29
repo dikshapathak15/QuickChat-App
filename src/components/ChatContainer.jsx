8daf426023134bd55e24ba1cb10cf316f4791e8c
dikshapathak15 <dikshapathak1541@gmail.com>
2025-12-29 22:12:22 +0530

    modified the sidebar

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
 
 const ChatContainer = () => {
   return (
-    <div><h1>ChatContainer</h1></div>
-  )
-}
+    <div>
+      <div>
+       <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
+       <p>Martin Johnson
+        <span className="w-2 h-2 rounded-full bg-green-500"></span>
+       </p>
+      </div>
+    </div>
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,31 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
 
 const HomePage = () => {
-
-    const[selectedUser, setSelecteduser] = useState(false);
+  const [selectedUser, setSelecteduser] = useState(false);
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
-      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md-grid-cols-2'}`}>
-      <Sidebar />
-      <ChatContainer/>
-      <RightSidebar/>
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-2 relative ${
+          selectedUser
+            ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+            : "md-grid-cols-2"
+        }`}
+      >
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelecteduser}
+        />
+        <ChatContainer
+          selectedUser={selectedUser}
+          setSelectedUser={setSelecteduser}
+        />
+        <RightSidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelecteduser}
+        />
       </div>
     </div>
   );
