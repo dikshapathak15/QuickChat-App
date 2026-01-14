b48fd65880870029244ed600260e1d2e8134cc91
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-14 18:24:45 +0530

    initiated the profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
-  return (
-    <div><h1>Profile Page</h1></div>
-  )
-}
 
-export default ProfilePage
+  const [selectedImg , setSelectedImg] = useState(null);
+  const [name , setName] = useState("Martin Jhonson");
+  const [ bio, setBio] = useState("Hii Everyone, I am using Quickchat");
+  const navigate = useNavigate();
+  return (
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profule Details</h3>
+          <label htmlFor="avatar" className="flex items-center gap-3">
+            <input type="file" id="avatar" accept=".png,.jpg,.jpeg"/>
+            <img src="" alt="" />
+          </label>
+        </form>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
