4852c51b0589e4a75001ba3c46ff8c4423cb7bc5
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-16 17:52:48 +0530

    added the backend

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+
+
+//create express app and http server
+const app = express();
+const server = http.createServer(app);
+
+//middleware setup
+app.use(express.json({limit:"4mb"}));
+app.use(cors()); ///enable all the url to connect with backend
+
+app.use('/api/status', (req,res)=> res.send("Server is live"))   //to check whether our backend server is running or not
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on port" + PORT));
