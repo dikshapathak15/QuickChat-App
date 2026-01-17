0fbecd3a45e485d0d09763ae208627fcbf23418c
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-17 10:16:33 +0530

    connected databse

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 
 //create express app and http server
@@ -14,5 +15,7 @@ app.use(cors()); ///enable all the url to connect with backend
 
 app.use('/api/status', (req,res)=> res.send("Server is live"))   //to check whether our backend server is running or not
 
+await connectDB();
+
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on port" + PORT));
+server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
