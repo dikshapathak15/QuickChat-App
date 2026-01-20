144d561c104faf8dbfb7497a7cc4ce8380329e4d
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-20 13:30:58 +0530

    added routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 
 //create express app and http server
@@ -13,7 +14,11 @@ const server = http.createServer(app);
 app.use(express.json({limit:"4mb"}));
 app.use(cors()); ///enable all the url to connect with backend
 
+
+//Route setup
 app.use('/api/status', (req,res)=> res.send("Server is live"))   //to check whether our backend server is running or not
+
+app.use("/api/auth", userRouter)
 
 await connectDB();
 
