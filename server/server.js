3ab5337f2c5d5e20804ea867fe78cdbb86a01499
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-20 14:25:10 +0530

    connected the socket.io

--- server/server.js
+++ server/server.js
@@ -5,22 +5,46 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/MessageRoutes.js";
-
+import { Server } from "socket.io";
 
 //create express app and http server
 const app = express();
 const server = http.createServer(app);
 
+//Initialized socket.io server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+//Store online users
+export const userSocketMap = {}; //{userId: socketId}
+
+//Socket.io connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //emit online users to all clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnected", (socket) => {
+    console.log("User disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
 //middleware setup
-app.use(express.json({limit:"4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors()); ///enable all the url to connect with backend
 
-
 //Route setup
-app.use('/api/status', (req,res)=> res.send("Server is live"))   //to check whether our backend server is running or not
+app.use("/api/status", (req, res) => res.send("Server is live")); //to check whether our backend server is running or not
 
-app.use("/api/auth", userRouter)
-app.use("/api/messages", messageRouter)
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 await connectDB();
 
