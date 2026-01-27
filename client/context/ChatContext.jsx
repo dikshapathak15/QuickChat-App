872d79288808a23235e84f575e9a4437d747eb00
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-27 11:25:42 +0530

    completed the chat context provider

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import toast from "react-hot-toast";
 
@@ -29,26 +29,63 @@ export const ChatProvider = ({ children }) => {
   const getMessages = async (userId) => {
     try {
       const { data } = await axios.get(`/api/messages/${userId}`);
-      if(data.success){
+      if (data.success) {
         setMessages(data.messages);
       }
     } catch (error) {
-        toast.error(error.message)
+      toast.error(error.message);
     }
   };
 
   //functiom to send message to selected user
   const sendMessage = async (messageData) => {
     try {
-        const {data} = await axios.get(`/api/messages/send/${selected._id}"`, messageData);
-        if(data.success){
-            setMessages((prevMessages) => [...prevMessages, data.newMessage])
-        }
+      const { data } = await axios.get(
+        `/api/messages/send/${selected._id}"`,
+        messageData,
+      );
+      if (data.success) {
+        setMessages((prevMessages) => [...prevMessages, data.newMessage]);
+      }
     } catch (error) {
-        toast.error(error.message);
+      toast.error(error.message);
     }
+  };
+
+  //function to subscribe to messages for selected user
+  const subscribeToMessages = async () => {
+    if (!socket) return;
+
+    socket.on("newMessages", (newMessage) => {
+      if (selectedUser && newMessage.senderId === selectedUser._id) {
+        newMessage.seen = true;
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+        axios.put(`/api/messages/mark/${newMessage._id}`);
+      } else {
+        setUnseenMessages((prevUnseenMessages) => ({
+          ...prevUnseenMessages,
+          [newMessage.senderId]: prevUnseenMessages[newMessage.senderId]
+            ? prevUnseenMessages[newMessage.senderId] + 1
+            : 1,
+        }));
+      }
+    });
+  };
+
+  //function to unsubscribe from messages
+  const unsubscribeFromMessages = () => {
+    if (socket) socket.off("newMessage");
+  };
+
+  useEffect(() => {
+    subscribeToMessages();
+    return () => unsubscribeFromMessages();
+  }, [socket, selectedUser]);
+
+  const value ={
+    messages, users, selectedUser,getUsers, setMessages, sendMessage, setSelectedUser, unseenMessages, setUnseenMessages
   }
 
-  const value = {};
+  
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
